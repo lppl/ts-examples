c9444d39f5a102dc409592f2b95e65380634d075
Łukasz Pietrek <lpietrek@gmail.com>
2023-10-21 14:10:53 +0200

    Update once fn

--- src/once.ts
+++ src/once.ts
@@ -10,16 +10,14 @@ export const once = <Fn extends (...args: any[]) => any>(fn: Fn): Fn => {
     }) as Fn;
 };
 
-
 let count = 0;
-let fn = once((val: string) => {
+const fn = once((val: string) => {
     count += 1;
     return `${val}:${count}`;
-}); 
+});
 
-const afterOne = fn('one'); 
-const afterTwo = fn('two');
+const afterOne = fn("one");
+const afterTwo = fn("two");
 
-
-console.assert(afterOne === 'one:1', afterOne);
-console.assert(afterTwo === 'one:1', afterTwo);
+console.assert(afterOne === "one:1", afterOne);
+console.assert(afterTwo === "one:1", afterTwo);
